c8e3a67e3a04a1510cb5ac4ed2a7e4d1eda803f8
TchTech <67865526+TchTech@users.noreply.github.com>
2022-04-21 16:07:26 +0300

    txts

--- src/sitemapper.py
+++ src/sitemapper.py
@@ -1,12 +1,10 @@
 import sys
-import logging
 from rucrawler import crawler
 
 if __name__ == '__main__':
     if '--iocp' in sys.argv:
         from asyncio import events, windows_events
         sys.argv.remove('--iocp')
-        logging.info('using iocp')
         el = windows_events.ProactorEventLoop()
         events.set_event_loop(el)
 
